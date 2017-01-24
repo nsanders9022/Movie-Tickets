8bd3cf4e6b6f69419e426d8805573c200538236a
Nicole Sanders and sandro mijatovic <nicole@>
2017-01-24 10:11:19 -0800

    add radio buttons for time and the movie names and will show a base price when the form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
-function Tickets(name) {
-  this.name = name;
+var ticketPrice = 12;
 
+function Tickets(name, age, time) {
+  this.myName = name;
+  this.age = age;
+  this.time = time;
+}
+
+Tickets.prototype.outputFormat = function() {
+  return this.myName + " " + this.age;
 }
 
 
 
 $(document).ready(function() {
+
   $("#movie-tickets").submit(function(event) {
     event.preventDefault();
 
-    var nameInput = $("#name").val();
+    var nameInput = $("input:radio[name=movieName]:checked").val();
+    var ageInput = parseInt($("#age").val());
+    var timeInput = $("input:radio[name=movieTime]:checked").val();
 
-    var newUser = new Tickets(nameInput)
 
-    $(".name").text(newUser.name);
+    var newUser = new Tickets(nameInput,ageInput, timeInput)
+
+    $(".price").text(ticketPrice);
   })
 })
