c40ad426753d499bfd0d5da98d72d8b62840ab40
Nicole Sanders and sandro mijatovic <nicole@>
2017-01-24 11:00:08 -0800

    reset the entire form when it is submitted and charge a higher price for newer movies and for prime time showings

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,44 @@ function Tickets(name, age, time) {
 
 Tickets.prototype.outputFormat = function() {
   if (this.age < 17 || this.age >= 65) {
-    ticketPrice -= 2
-  }
-  return ticketPrice
+    ticketPrice -= 2;
+  };
+  if (this.time === 2) {
+    ticketPrice += 3;
+  };
+  if (this.myName === 2) {
+    ticketPrice += 2;
+  };
+  return ticketPrice;
+};
+
+Tickets.prototype.reset1 = function() {
+  this.myName = "";
+  this.age = "";
+  this.time = "";
 }
-
-
 
 $(document).ready(function() {
 
   $("#movie-tickets").submit(function(event) {
     event.preventDefault();
 
-    var nameInput = $("input:radio[name=movieName]:checked").val();
+    var nameInput = parseInt($("input:radio[name=movieName]:checked").val());
     var ageInput = parseInt($("#age").val());
-    var timeInput = $("input:radio[name=movieTime]:checked").val();
+    var timeInput = parseInt($("input:radio[name=movieTime]:checked").val());
+
 
 
     var newUser = new Tickets(nameInput, ageInput, timeInput)
 
     $(".price").text(newUser.outputFormat());
+
+    $('input[name=movieName]').prop('checked',false);
+    $("#age").val("");
+    $('input[name=movieTime').prop('checked',false);
+    
+    ticketPrice = 12;
+    // newUser = newUser.reset1();
+
   })
 })
